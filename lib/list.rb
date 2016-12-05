8eaf1cfec908d9ee89de119ee7701736d37ac96e
Caitlin Ashtari and Marty Kovach <caitlin@>
2016-12-05 11:16:38 -0800

    add save and == methods with passing specs.

--- lib/list.rb
+++ lib/list.rb
@@ -16,4 +16,13 @@ class List
     end
     lists
   end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO lists (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  define_method(:==) do |another_list|
+    self.name().==(another_list.name()).&(self.id().==(another_list.id()))
+  end
 end
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -16,4 +16,20 @@ describe(List) do
       expect(List.all()).to(eq([]))
     end
   end
+
+  describe('#save') do
+    it('lets you save lists to the database') do
+      list = List.new({:name => "Epicodus stuff", :id => nil})
+      list.save()
+      expect(List.all()).to(eq([list]))
+    end
+  end
+
+  describe("#==") do
+    it("is the same list if it has the same name") do
+      list1 = List.new({:name => "Epicodus stuff", :id => nil})
+      list2 = List.new({:name => "Epicodus stuff", :id => nil})
+      expect(list1).to(eq(list2))
+    end
+  end
 end
