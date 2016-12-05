653f25752b4a5ef4bff56e3bacd050410a31870b
Caitlin Ashtari and Marty Kovach <caitlin@>
2016-12-05 10:27:04 -0800

    add passing specs for to_do.

--- lib/task.rb
+++ lib/task.rb
@@ -1,8 +1,19 @@
 class Task
   attr_reader :description
-
+  @@all_tasks = []
   define_method(:initialize) do |attributes|
     @description = attributes.fetch(:description)
   end
 
+  define_singleton_method(:all) do
+    @@all_tasks
+  end
+
+  define_method(:save) do
+    @@all_tasks.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@all_tasks = []
+  end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,11 +1,43 @@
 require('rspec')
 require('task')
+require('pg')
 
-describe(Task) do
-  describe('#description') do
-    it('return the description of the task') do
-      new_task = Task.new({:description => "scrub the zebra"})
-      expect(new_task.description()).to(eq("scrub the zebra"))
-    end
+DB = PG.connect({:dbname => 'to_do_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM tasks *;")
   end
+end
+
+  describe(Task) do
+    describe('#description') do
+      it('return the description of the task') do
+        new_task = Task.new({:description => "scrub the zebra"})
+        expect(new_task.description()).to(eq("scrub the zebra"))
+      end
+    end
+
+  describe(".all") do
+      it("is empty at first") do
+        expect(Task.all()).to(eq([]))
+      end
+    end
+
+    describe("#save") do
+      it("add a task to the array of saved tasks") do
+        test_task = Task.new({:description => "wash the lion"})
+        test_task.save()
+        expect(Task.all()).to(eq([test_task]))
+      end
+    end
+
+    describe(".clear") do
+      it("empties out all of the saved tasks") do
+        Task.new({:description => "wash the lion"}).save()
+        Task.clear()
+        expect(Task.all()).to(eq([]))
+      end
+    end
+
 end
