8a394c94880876926b00f5dfed58005e3d5bc3ce
Caitlin Ashtari <caitlinashtari@gmail.com>
2016-12-08 13:22:19 -0800

    add passing tasks method to return all tasks in a list.

--- lib/list.rb
+++ lib/list.rb
@@ -34,5 +34,17 @@ class List
         end
       end
       found_list
-  end  
+  end
+
+  define_method(:tasks) do
+    list_tasks = []
+    tasks = DB.exec("SELECT * FROM tasks WHERE list_id = #{self.id};")
+    tasks.each do |task|
+      description = task.fetch("description")
+      list_id = task.fetch("list_id").to_i
+      due_date = task.fetch("due_date")
+      list_tasks.push(Task.new({:description => description, :list_id => list_id, :due_date => due_date}))
+    end
+    list_tasks
+  end
 end
